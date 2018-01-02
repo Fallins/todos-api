18c2d8bbe1438bad7f7195524e225706c928a76d
Ben.Liao <ben.liao@sapphireinfo.com.tw>
2018-01-02 16:38:04 +0800

    add feature user login

--- server/server.js
+++ server/server.js
@@ -104,7 +104,7 @@ app.post('/users', (req, res) => {
   user.save().then(()=>{
     return user.generateAuthToken()
   }).then(token => {
-    console.log({user})
+    // console.log({user})
     res.header('x-auth', token).send(user)
   }).catch( e => res.status(400).send(e))
 })
@@ -113,6 +113,17 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user)
 })
 
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password'])
+
+  User.findByCredentials(body.email, body.password).then( user => {
+    return user.generateAuthToken().then(token => {
+      res.header('x-auth', token).send(user)
+    })
+  }).catch( e => {
+    res.status(400).send()
+  })
+})
 
 
 
